5bc194aac1494dff896c642e1809268a5f9980b8
marc berger <marc.berger75020@gmail.com>
2018-07-08 21:35:29 +0200

    add competences

--- articles.js
+++ articles.js
@@ -13,15 +13,18 @@ const articles =
         <div class="main_list">
         <ul>
             <h3 class="list-title">Projets personnels :</h3>
-            <li><span><a href="https://github.com/mberger75/myOpenLogBook">My OpenLogBook</a></span></li>
-            <li><span><a href="https://github.com/mberger75/twitch_fcc">Twitch streamers</a></span></li>
-            <li><span><a href="#">Finger Riot</a></span></li>
+            <li><span><a href="https://codepen.io/evod/full/odGGMY/" target="_blank">Twitch API</a></span></li>
+            <li><span><a href="https://codepen.io/evod/full/vjemJx/" target="_blank">Wikipedia API</a></span></li>
+            <li><span><a href="https://codepen.io/evod/full/zjEGRx/" target="_blank">Application Météo</a></span></li>
+            <li><span><a href="https://codepen.io/evod/full/LmzPQR/" target="_blank">Générateur de Citations</a></span></li>
+            <li><span><a href="https://github.com/mberger75/metalhead" target="_blank">Metalhead query-selectors</a></span></li>
         </ul>
         </div>
         <div class="main_list">
         <ul>
             <h3 class="list-title">Projets d'étude :</h3>
-            <li><span><a href="https://www.bergermarc.com/chaletscaviar/">Chalets et Caviar</a></span></li>
+            <li><span><a href="https://www.bergermarc.com/chaletscaviar/" target="_blank">Chalets et Caviar</a></span></li>
+            <li><span><a href="https://github.com/mberger75/myOpenLogBook" target="_blank">My OpenLogBook</a></span></li>
         </ul>
         </div>
         `
@@ -34,17 +37,23 @@ const articles =
         <ul>
             <h3 class="list-title">Technos :</h3>
             <li><span>HTML / CSS</span></li>
-            <li><span>Javascript</span></li>
+            <li><span>Bootstrap / Materialize / Bulma</span></li>
+            <li><span>Vanilla Javascript</span></li>
+            <li><span>Node.js / JQuery / ES6</span></li>
+            <li><span>Java</span></li>
             <li><span>PHP / MySQL</span></li>
             <li><span>Wordpress</span></li>
+            <li><span>Yoast SEO / Elementor</span></li>
         </ul>
         </div>
         <div class="main_list">
         <ul>
-            <h3 class="list-title">Logiciels :</h3>
+            <h3 class="list-title">Outils :</h3>
             <li><span>Visual Studio Code</span></li>
             <li><span>Git</span></li>
+            <li><span>Trello</span></li>
             <li><span>FileZilla</span></li>
+            <li><span>WAMP</span></li>
             <li><span>Adobe Illustrator</span></li>
             <li><span>Adobe Photoshop</span></li>
         </ul>
@@ -55,9 +64,10 @@ const articles =
         title: `À PROPOS DE MOI`,
         para:
         `
-        <p>Autodidacte depuis plus de 2 ans, je valide actuellement mes compétences par le biais du cursus <a href="https://openclassrooms.com/paths/60-developpeur-dapplication-frontend" target="_blank">"Développeur d'Application"</a> sur <a href="https://openclassrooms.com/" target="_blank">OpenClassrooms.</a><br>
-        Cette formation mène à un Titre RCNP niveau II (équivalent à Bac+3).<p>
-        <p>Passionné et créatif, je passe mon temps à élaborer site internet, jeux-vidéos, illustrations et bien d'autres choses ...</p>
+        <img class="profilPicture" src="https://image.ibb.co/fQiGGd/photo_Profil.png" alt="photo_Profil">
+        <p>Autodidacte depuis plus de 2 ans, je valide actuellement mes compétences par le biais du cursus <a href="https://openclassrooms.com/paths/60-developpeur-dapplication-frontend" target="_blank">"Développeur d'Application"</a> sur <a href="https://openclassrooms.com/" target="_blank">OpenClassrooms</a>.<br>
+        Cette formation mène à un <a href="http://www.rncp.cncp.gouv.fr/grand-public/visualisationFiche?format=fr&fiche=27099" target="_blank">Titre RCNP</a> niveau II (équivalent à Bac+3).<p>
+        <p>Passionné et créatif, je passe mon temps à élaborer sites internet, jeux-vidéos, illustrations et bien d'autres choses ...</p>
         `
     },
     {
