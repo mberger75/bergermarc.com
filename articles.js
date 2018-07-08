8d5dc825d77cd2e3a089de22b8b1748c6de9cef7
marc berger <marc.berger75020@gmail.com>
2018-07-09 01:05:56 +0200

    add articles : webdesign & annexe

--- articles.js
+++ articles.js
@@ -36,26 +36,37 @@ const articles =
         <div class="main_list">
         <ul>
             <h3 class="list-title">Technos :</h3>
-            <li><span>HTML / CSS</span></li>
-            <li><span>Bootstrap / Materialize / Bulma</span></li>
-            <li><span>Vanilla Javascript</span></li>
-            <li><span>Node.js / JQuery / ES6</span></li>
+            <li><span>HTML / CSS*</span></li>
+            <li><span>Vanilla Javascript* / ES6</span></li>
             <li><span>Java</span></li>
             <li><span>PHP / MySQL</span></li>
-            <li><span>Wordpress</span></li>
-            <li><span>Yoast SEO / Elementor</span></li>
+            <li><span>Wordpress*</span></li>
         </ul>
         </div>
         <div class="main_list">
         <ul>
             <h3 class="list-title">Outils :</h3>
             <li><span>Visual Studio Code</span></li>
-            <li><span>Git</span></li>
-            <li><span>Trello</span></li>
+            <li><span>Git / <a href="https://github.com/mberger75" target="_blank">Github</a></span></li>
+            <li><span><a href="https://trello.com/" target="_blank">Trello</a></span></li>
             <li><span>FileZilla</span></li>
             <li><span>WAMP</span></li>
+        </ul>
+        </div>
+        <div class="main_list">
+        <ul>
+            <h3 class="list-title">Webdesign :</h3>
             <li><span>Adobe Illustrator</span></li>
             <li><span>Adobe Photoshop</span></li>
+            <li><span><a href="https://css-tricks.com/" target="_blank">CSS-TRICKS</a></span></li>
+        </ul>
+        </div>
+        <div class="main_list">
+        <ul>
+            <h3 class="list-title">Annexe :</h3>
+            <li><span>JS* -> Node.js / JQuery</span></li>
+            <li><span>CSS* -> Bootstrap / Bulma</span></li>
+            <li><span>W* -> Yoast SEO/ Elementor</span></li>
         </ul>
         </div>
         `
@@ -65,8 +76,9 @@ const articles =
         para:
         `
         <img class="profilPicture" src="https://image.ibb.co/fQiGGd/photo_Profil.png" alt="photo_Profil">
-        <p>Autodidacte depuis plus de 2 ans, je valide actuellement mes compétences par le biais du cursus <a href="https://openclassrooms.com/paths/60-developpeur-dapplication-frontend" target="_blank">"Développeur d'Application"</a> sur <a href="https://openclassrooms.com/" target="_blank">OpenClassrooms</a>.<br>
-        Cette formation mène à un <a href="http://www.rncp.cncp.gouv.fr/grand-public/visualisationFiche?format=fr&fiche=27099" target="_blank">Titre RCNP</a> niveau II (équivalent à Bac+3).<p>
+        <p>Autodidacte depuis plus de 2 ans, j'ai appris la création de site internet et le webdesign à travers des bootcamps, des livres et divers tutoriels.<br><br>
+        Actuellement, je développe et valide mes compétences par le biais du cursus <a href="https://openclassrooms.com/paths/60-developpeur-dapplication-frontend" target="_blank">"Développeur d'Application"</a> sur <a href="https://openclassrooms.com/" target="_blank">OpenClassrooms</a>.<br>
+        Cette formation d'un an mène à un <a href="http://www.rncp.cncp.gouv.fr/grand-public/visualisationFiche?format=fr&fiche=27099" target="_blank">Titre RCNP</a> niveau II (Bac+4).<p>
         <p>Passionné et créatif, je passe mon temps à élaborer sites internet, jeux-vidéos, illustrations et bien d'autres choses ...</p>
         `
     },
