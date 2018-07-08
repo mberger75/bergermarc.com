662f588473899fba2c61b707b1d7a5e917a0712d
marc berger <marc.berger75020@gmail.com>
2018-07-08 17:38:48 +0200

    add A propos de moi, media queries for header

--- articles.js
+++ articles.js
@@ -8,9 +8,8 @@ const articles =
     {
         title: `ACCUEIL`,
         para:
-        `<strong>Voici les projets que j'ai réalisé cette année.</strong><br><br>
-        Projets personnels :
-
+        `<p style="text-align: center;">Voici les projets que j'ai réalisé cette année 2018.</p><br>
+        <strong>Projets personnels</strong> :
         <ul>
             <li><span><a href="https://github.com/mberger75/myOpenLogBook">My OpenLogBook</a></span></li>
             <li><span><a href="https://github.com/mberger75/twitch_fcc">Twitch streamers</a></span></li>
@@ -21,5 +20,12 @@ const articles =
         <ul>
             <li><span><a href="https://www.bergermarc.com/chaletscaviar/">Chalets et Caviar</a></span></li>
         </ul>`
-    }
+    },
+    {
+        title: `À PROPOS DE MOI`,
+        para:
+        `<p>Autodidacte depuis plus de 2 ans, je valide actuellement mes compétences par le biais du cursus <a href="https://openclassrooms.com/paths/60-developpeur-dapplication-frontend" target="_blank">"Développeur d'Application"</a> sur <a href="https://openclassrooms.com/" target="_blank">OpenClassrooms.</a><br>
+        Cette formation mène à un Titre RCNP niveau II (équivalent à Bac+3).<p>
+        <p>Passionné et créatif, je passe mon temps à élaborer site internet, jeux-vidéos, illustrations et bien d'autres choses ...</p>`
+    },
 ]
