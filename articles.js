e3de6f23dc1e6d0b4676419251e4c4add1b1a7f4
marc berger <marc.berger75020@gmail.com>
2018-07-08 20:29:46 +0200

    change some classes

--- articles.js
+++ articles.js
@@ -8,36 +8,70 @@ const articles =
     {
         title: `PROJETS`,
         para:
-        `<p style="text-align: center;">Voici les projets que j'ai réalisé cette année 2018.</p><br>
-        <strong>Projets personnels</strong> :
+        `
+        <p style="text-align: center;">Voici mes projets réalisés en 2018.</p>
+        <div class="main_list">
         <ul>
+            <h3 class="list-title">Projets personnels :</h3>
             <li><span><a href="https://github.com/mberger75/myOpenLogBook">My OpenLogBook</a></span></li>
             <li><span><a href="https://github.com/mberger75/twitch_fcc">Twitch streamers</a></span></li>
             <li><span><a href="#">Finger Riot</a></span></li>
         </ul>
-        <strong>Projets OpenClassrooms</strong> :
-
+        </div>
+        <div class="main_list">
         <ul>
+            <h3 class="list-title">Projets d'étude :</h3>
             <li><span><a href="https://www.bergermarc.com/chaletscaviar/">Chalets et Caviar</a></span></li>
-        </ul>`
+        </ul>
+        </div>
+        `
+    },
+    {
+        title: `COMPÉTENCES`,
+        para:
+        `
+        <div class="main_list">
+        <ul>
+            <h3 class="list-title">Technos :</h3>
+            <li><span>HTML / CSS</span></li>
+            <li><span>Javascript</span></li>
+            <li><span>PHP / MySQL</span></li>
+            <li><span>Wordpress</span></li>
+        </ul>
+        </div>
+        <div class="main_list">
+        <ul>
+            <h3 class="list-title">Logiciels :</h3>
+            <li><span>Visual Studio Code</span></li>
+            <li><span>Git</span></li>
+            <li><span>FileZilla</span></li>
+            <li><span>Adobe Illustrator</span></li>
+            <li><span>Adobe Photoshop</span></li>
+        </ul>
+        </div>
+        `
     },
     {
         title: `À PROPOS DE MOI`,
         para:
-        `<p>Autodidacte depuis plus de 2 ans, je valide actuellement mes compétences par le biais du cursus <a href="https://openclassrooms.com/paths/60-developpeur-dapplication-frontend" target="_blank">"Développeur d'Application"</a> sur <a href="https://openclassrooms.com/" target="_blank">OpenClassrooms.</a><br>
+        `
+        <p>Autodidacte depuis plus de 2 ans, je valide actuellement mes compétences par le biais du cursus <a href="https://openclassrooms.com/paths/60-developpeur-dapplication-frontend" target="_blank">"Développeur d'Application"</a> sur <a href="https://openclassrooms.com/" target="_blank">OpenClassrooms.</a><br>
         Cette formation mène à un Titre RCNP niveau II (équivalent à Bac+3).<p>
-        <p>Passionné et créatif, je passe mon temps à élaborer site internet, jeux-vidéos, illustrations et bien d'autres choses ...</p>`
+        <p>Passionné et créatif, je passe mon temps à élaborer site internet, jeux-vidéos, illustrations et bien d'autres choses ...</p>
+        `
     },
     {
         title: `ME CONTACTER`,
         para:
-        `<div id="form">
+        `
+        <div id="form">
         <form method="POST" action="https://formspree.io/marc.berger75020@gmail.com">
             <input type="name" name="name" placeholder="Nom">
             <input type="email" name="email" placeholder="Email">
             <textarea name="message" placeholder="Message"></textarea>
             <button type="submit">ENVOYER</button>
         </form>
-        </div>`
+        </div>
+        `
     },
 ]
